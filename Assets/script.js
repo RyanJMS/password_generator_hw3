ed3c0acf022c1bdbcd2f4e7a08095e165c0d0023
RyanJMS <59546790+RyanJMS@users.noreply.github.com>
2020-02-29 13:28:15 -0500

    fixed bug with only selecting 2 options, or no options

--- Assets/script.js
+++ Assets/script.js
@@ -2,94 +2,95 @@
 
 //Prompt for User Password Length
 
-
 //Initializing 4 char sets for future user confirms
-const lowerCase = "abcdefghijklmnopqrstuvwxyz"
-const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-const numbers = "1234567890"
-const special = "!@#$%^&*()"
+const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const numbers = "1234567890";
+const special = "!@#$%^&*()";
 
 //Init passLength to hold password length
-let passLength
+let passLength;
 
 //Init charSet to hold character set determined by user confirms
 
-
-
 //Init user confirms
-let confirmLower
-let confirmUpper
-let confirmNumbers
-let confirmSepcial
-
+let confirmLower;
+let confirmUpper;
+let confirmNumbers;
+let confirmSepcial;
 
 //If user input = true, add it to charSet
 function generatePassword() {
-  let pass = ""
-  let charSet = ""
+  let pass = "";
+  let charSet = "";
   //Take user prompt and convert to integer
-  passLength = parseInt(prompt("How many characters do you want your password to be? (Must be between 8 and 128)"))
+  passLength = parseInt(
+    prompt(
+      "How many characters do you want your password to be? (Must be between 8 and 128)"
+    )
+  );
 
   //Make sure the condition only runs if the user inputs a length between 8 and 128
   //If passLength is less than 8 and greater than 128, alert user to try again, restart function
-  while ((passLength < 8 || passLength > 128)||(isNaN(passLength))){
-    alert("Please enter a number between 8 and 128")
-    passLength = parseInt(prompt("How many characters do you want your password to be? (Must be between 8 and 128)"))
+  while (passLength < 8 || passLength > 128 || isNaN(passLength)) {
+    alert("Please enter a number between 8 and 128");
+    passLength = parseInt(
+      prompt(
+        "How many characters do you want your password to be? (Must be between 8 and 128)"
+      )
+    );
   }
   //If a user enters something that is not a number, alert user to try again, restart function
-  
-{
 
-  //User confirms
+  {
+    //User confirms
 
-    confirmLower = confirm("Do you want lowercase letters?")
-    confirmUpper = confirm("Do you want uppercase letters?")
-    confirmNumbers = confirm("Do you want numbers?")
-    confirmSpecial = confirm("Do you want special characters?")
-  
-  //If user confirm = true, add to charSet
-  if (confirmLower) {
-    charSet += lowerCase
-  }
-  if (confirmUpper) {
-    charSet += upperCase
-  }
-  if (confirmNumbers) {
-    charSet += numbers
-  }
-  if (confirmSpecial) {
-    charSet += special
-  }
-  //If a user selects nothing
-  else {
-    alert("Please select a character type")
-    generatePassword()
+    confirmLower = confirm("Do you want lowercase letters?");
+    confirmUpper = confirm("Do you want uppercase letters?");
+    confirmNumbers = confirm("Do you want numbers?");
+    confirmSpecial = confirm("Do you want special characters?");
+
+    //If user confirm = true, add to charSet
+    if (confirmLower === true) {
+      charSet += lowerCase;
+    }
+    if (confirmUpper === true) {
+      charSet += upperCase;
+    }
+    if (confirmNumbers === true) {
+      charSet += numbers;
+    }
+    if (confirmSpecial === true) {
+      charSet += special;
+    }
+    //If a user selects nothing
+    if (
+      confirmLower === false &&
+      confirmUpper === false &&
+      confirmNumbers === false &&
+      confirmSpecial === false
+    ) {
+      alert("Please select a chacter type");
+    } else {
+      //Loop until passLength is reached, pass(final password) takes charset (as determined by user) and randomizes to the length of the charset
+      for (let i = 0; i < passLength; i++) {
+        pass += charSet.charAt(Math.floor(Math.random() * charSet.length));
+      }
+      //returns the final password
+    }
   }
 
-  //Loop until passLength is reached, pass(final password) takes charset (as determined by user) and randomizes to the length of the charset
-  for(let i = 0; i < passLength; i++) {
-    pass += charSet.charAt(Math.floor(Math.random() * charSet.length))
-  }
-  //returns the final password
-  return pass
-  }
-  //
+  return pass;
 }
-
-
-
 
 //Generate password Button
 let generateBtn = document.querySelector("#generate");
-
-
 
 // Write password to the #password input
 function writePassword() {
   let password = generatePassword();
   let passwordText = document.querySelector("#password");
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
